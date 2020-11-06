f8e3c44caf8728803d1249a5440d70b786c7f4a5
CassiaVaz <54650944+CassiaVaz@users.noreply.github.com>
2020-11-06 21:19:02 +0530

    Add UI for manufacturer

--- blockchain/src/components/manufacturer/Manufacturer.js
+++ blockchain/src/components/manufacturer/Manufacturer.js
@@ -1,5 +1,51 @@
 import React, { Component } from 'react';
-function Manufacturer(){
-    
+import Sample from '../../components/images/supplierbg.jpg';
+import AddMedicine from './AddMedicine'
+import Header from '../../components/header/Header';
+import {NavLink, withRouter, BrowserRouter as Router, Route} from 'react-router-dom'; 
+import history from '../../components/views/history';
+import Button from '@material-ui/core/Button';
+import { Grid } from '@material-ui/core';
+
+class Manufacturer extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            addMedicineClick: false
+        }
+    }
+    handleAddMedicineClick(){
+        this.setState({
+            addMedicineClick: true
+        })
+        console.log("Add Medicine");
+    }
+    render(){
+        return (
+            <Router>
+              <div style= {{ backgroundImage: `url(${Sample})`,
+                              backgroundSize: "cover", backgroundRepeat: "no-repeat", height: '1000px',
+                              }}>
+                <Header/>
+                <div className="body-container">
+                  <h3 style={{ textAlign: "center", color: "white" }}>Welcome Manufacturer!</h3>
+                  <Grid container>
+                      <Grid item md={4}>
+                        <Button variant="contained" color="primary" onClick = {()=> this.handleAddMedicineClick()}>Add New Medicine</Button>
+                        <div className= "button-clicked">{(this.state.addMedicineClick)? <AddMedicine/> : ''}</div>
+                      </Grid>
+                      <Grid item md={4}>
+                        <Button variant="contained" color="primary" >View Raw Material</Button>
+                        {/* <div className= "button-clicked">{(this.state.addMedicineClick)? <AddMedicine/> : ''}</div> */}
+                      </Grid>
+                   
+                  </Grid>
+
+                </div>
+              </div>
+            </Router>
+          );
+
+    }
 }
 export default Manufacturer
