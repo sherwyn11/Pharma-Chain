3fcc0cbd79ca5ab4056b7112bc27465ee2b0312c
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-11-14 00:41:15 +0530

    :hammer: added timestamp to txns

--- blockchain/contracts/Transactions.sol
+++ blockchain/contracts/Transactions.sol
@@ -10,27 +10,28 @@ contract Transactions {
         address fromAddr;
         address toAddr;
         bytes32 prevTxn;
+        uint timestamp;
     }
     
     mapping(uint => txns) public transactions;
     uint public txnCount = 0;
-
     
     constructor(address _creator) public {
         Creator = _creator;
     }
     
-    event txnCreated(bytes32 _txnHash, address _from, address _to, bytes32 _prev);
+    event txnCreated(bytes32 _txnHash, address _from, address _to, bytes32 _prev, uint _timestamp);
     
     function createEntry(bytes32 _txnHash, address _from, address _to, bytes32 _prev) public {
+        uint _timestamp = now;
         if(txnCount == 0) {
-            transactions[txnCount] = txns(_txnHash, _from, _to, _prev);
+            transactions[txnCount] = txns(_txnHash, _from, _to, _prev, _timestamp);
         } else {
             require(transactions[txnCount - 1].txnHash == _prev, "Transaction error occurred!");
-            transactions[txnCount] = txns(_txnHash, _from, _to, _prev);
+            transactions[txnCount] = txns(_txnHash, _from, _to, _prev, _timestamp);
         }
         txnCount += 1;
-        emit txnCreated(_txnHash, _from, _to, _prev);
+        emit txnCreated(_txnHash, _from, _to, _prev, _timestamp);
     }
 
     function getAllTransactions() public view returns(txns[] memory) {
