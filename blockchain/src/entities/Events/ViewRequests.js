01500ab650dc9df87f87b6e0cb6beb86ddbf3bb5
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-11-19 01:17:38 +0530

    :sparkles: cleant code

--- blockchain/src/entities/Events/ViewRequests.js
+++ blockchain/src/entities/Events/ViewRequests.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Loader from '../../components/Loader';
-import Transactions from '../../build/Transactions.json';
 
 const useStyles = makeStyles((theme) => ({
   root: {
